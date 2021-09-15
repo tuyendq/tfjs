c39ce3fc9e1cf59ae20b915a1f5d30920e72e4c6
Tuyen Dang <tuyendq@gmail.com>
2021-09-15 21:04:25 +0700

    Add iris_classifier.html, iris_classifier.s

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Learn TensorFlow.js
 
+[Iris Data Set](https://archive.ics.uci.edu/ml/datasets/iris)
+
 ## Courses
 
 [Browser-based Models with TensorFlow.js | DeepLearning.AI](https://www.coursera.org/learn/browser-based-models-tensorflow/)
---
+++ src/iris_classifier.js
@@ -0,0 +1,58 @@
+'use strict';
+
+console.log("Hello TensorFlow.js: Iris classifier");
+console.log(tf.version);
+
+(async function run() {
+  const csvUrl = 'data/iris.csv';
+  const trainingData = tf.data.csv(csvUrl, {
+    columnConfigs: {
+      species: {
+        isLabel: true
+      }
+    }
+  });
+
+  console.log(trainingData);
+
+  const convertedData = trainingData.map(({xs, ys}) => {
+    const labels = [
+      ys.species == 'setosa' ? 1 : 0,
+      ys.species == 'virginica' ? 1 : 0,
+      ys.species == 'versicolor' ? 1:0
+    ]
+    return { xs:Object.values(xs), ys: Object.values(labels)};
+  }).batch(10);
+
+  const model = tf.sequential();
+  model.add(tf.layers.dense({
+    // inputShape: [numOfFeatures],
+    inputShape: [4],
+    activation: "sigmoid", 
+    units: 5
+  }));
+  model.add(tf.layers.dense({
+    activation: "softmax",
+    units: 3
+  }));
+  model.compile({
+    loss: "categoricalCrossentropy",
+    optimizer: tf.train.adam(0.06)
+  });
+
+  await model.fitDataset(
+    convertedData,
+    {
+      epochs: 100,
+      callbacks: {
+        onEpochEnd: async(epoch, logs) => {
+          console.log("E: " + epoch + " Loss: " + logs.loss);
+        }
+      }
+    }
+  );
+
+  const testVal = tf.tensor2d([5.8, 2.7, 5.1, 1.9], [1, 4]);
+  const prediction = model.predict(testVal);
+  alert(prediction);
+})();
